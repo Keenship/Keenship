4ee9217963f7883a251c8638402bdea602acd4da
mudler <mudler@dark-lab.net>
2014-11-20 00:00:28 +0100

    Enabling loading plugins from config file

--- lib/Keenship/Plugin/Bootstrap.pm
+++ lib/Keenship/Plugin/Bootstrap.pm
@@ -3,11 +3,17 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 sub register {
     my ( $self, $app ) = @_;
+
     # Load basilar must-have plugin
     $app->plugin("AssetPack");
     $app->plugin("Bootstrap3");
     $app->plugin("FontAwesome4");
     $app->plugin("JQuery");
+
+    return unless ( exists $app->config->{Plugins} );
+
+    $app->plugin( $Plugin, $app->config->{Plugins}->{$Plugin} )
+        foreach ( my $Plugin = %{ $app->config->{Plugins} } );
 }
 
 !!42;
