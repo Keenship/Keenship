1a4d14c3b1873806aeb375ac9da1979370fd494e
mudler <mudler@dark-lab.net>
2014-12-18 11:17:32 +0100

    supporting pinto in depinstall

--- lib/Keenship.pm
+++ lib/Keenship.pm
@@ -11,7 +11,7 @@ BEGIN {
     unshift @INC, cwd . "/lib";
 }
 
-our $VERSION  = "0.08";
+our $VERSION  = "0.09";
 our $CODENAME = "Rosetta";
 
 has 'keenship_home' =>
--- lib/Keenship/Command/depinstall.pm
+++ lib/Keenship/Command/depinstall.pm
@@ -2,16 +2,17 @@ package Keenship::Command::depinstall;
 use Mojo::Base 'Mojolicious::Command';
 use Carp qw(croak);
 has description => 'Install the deps of the application with cpanm.';
-has usage       => "Usage: APPLICATION depinstall [APP]\n";
+has usage       => "Usage: APPLICATION depinstall [APP] [MIRROR]\n";
 
 sub run {
-    my ( $self, $application, @args ) = @_;
+    my ( $self, $application, $mirror ) = @_;
     croak "Fatal error: you must supply an applicaiton name"
         if !$application;
-    $self->app->keenship_home->rel_dir($application);
-    exec(     "cd "
-            . $self->app->keenship_home->rel_dir($application)
-            . ";cpanm --installdeps . @args" );
+    exec( "cd "
+            . $self->app->keenship_home->rel_dir($application) . "; "
+            . ( defined $mirror )
+        ? "cpanm --mirror '$mirror' --installdeps . @_"
+        : "cpanm --installdeps . @_" );
 }
 
 !!42;
