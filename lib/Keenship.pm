542619e8479679e4c5ee45074cc7d77479e42d04
mudler <mudler@dark-lab.net>
2014-12-19 11:35:59 +0100

    added pull command

--- lib/Keenship.pm
+++ lib/Keenship.pm
@@ -11,7 +11,7 @@ BEGIN {
     unshift @INC, cwd . "/lib";
 }
 
-our $VERSION  = "0.20";
+our $VERSION  = "0.21";
 our $CODENAME = "Rosetta";
 
 has 'keenship_home' =>
---
+++ lib/Keenship/Command/pull.pm
@@ -0,0 +1,18 @@
+package Keenship::Command::pull;
+use Mojo::Base 'Mojolicious::Command';
+use Keenship::Util qw(git_repo_name);
+use Git::Repository;
+has description => 'Update a Keenship cartridge.';
+has usage       => "Usage: APPLICATION pull [APP]\n";
+
+sub run {
+    my ( $self, $name ) = @_;
+    my $destination = $self->app->keenship_home->rel_dir($name);
+    say "Cloning/Pulling $url into " . $destination;
+    return Git::Repository->new( work_tree => $destination )->run("pull")
+        if -d $destination;
+    Git::Repository->run( clone => $url, $destination );
+
+}
+
+1;
