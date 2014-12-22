48aba2e3e69031ab92c023b270344c2ed288df5b
mudler <mudler@dark-lab.net>
2014-12-22 23:09:24 +0100

    fix, bump

--- lib/Keenship/Command/restart.pm
+++ lib/Keenship/Command/restart.pm
@@ -8,8 +8,8 @@ has usage       => "Usage: restart <appname> [opts]\n";
 
 sub run {
     my $self = shift;
-    Keenship::Command::stop->new->run(@_);
-    Keenship::Command::plack->new->run(@_);
+    Keenship::Command::stop->new->app($self->app)->run(@_);
+    Keenship::Command::plack->new->app($self->app)->run(@_);
 }
 
 1;
